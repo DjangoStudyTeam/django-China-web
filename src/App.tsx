167ee12faee5a8901902365f1212a64afeaf5efa
kijoenn <suyoumaner@foxmail.com>
2022-01-06 01:03:24 +0800

    feat: add basic fuctions (.vscode / pnpm / react router)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import About from './pages/About';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Test from './pages/Test';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Test />} />
+      </Routes>
+    </Router>
   );
 }
 
